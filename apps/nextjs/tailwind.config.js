b71b1aee93fa8929a75626708df62d18ccc3efbf
Erko Bridee <erko.bridee@gmail.com>
2020-11-26 18:20:55 +0100

    feat(tailwind): add purge css config

--- apps/nextjs/tailwind.config.js
+++ apps/nextjs/tailwind.config.js
@@ -1,3 +1,10 @@
 const rootConfig = require('../../tailwind.config.js');
 
-module.exports = Object.assign(rootConfig, {});
+const localConfig = Object.assign(rootConfig, {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [`${__dirname}/src/**/*.{jsx,tsx}`],
+  },
+});
+
+module.exports = localConfig;
