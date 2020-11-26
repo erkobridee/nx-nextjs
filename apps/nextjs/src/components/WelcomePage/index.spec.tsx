0031551c6de5e3a2f04b9223221c45a9d308c084
Erko Bridee <erko.bridee@gmail.com>
2020-11-26 12:39:00 +0100

    update(pages): index page
    move it's content to a component

--- apps/nextjs/src/components/WelcomePage/index.spec.tsx
+++ apps/nextjs/src/components/WelcomePage/index.spec.tsx
@@ -3,11 +3,11 @@
 
 import { render } from '@testing-library/react';
 
-import Index from '../pages/index';
+import WelcomePage from './index';
 
-describe('Index', () => {
+describe('WelcomePage', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
+    const { baseElement } = render(<WelcomePage />);
     expect(baseElement).toBeTruthy();
   });
 });
