460877ca372710bb34c38d78dba3ec72df8f04e6
Erko Bridee <erko.bridee@gmail.com>
2020-11-26 14:29:07 +0100

    update(apps/nextjs/pages): code on _app.tsx

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,5 +1,4 @@
-// it's not needed with React v17
-// import React from 'react';
+import React from 'react';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -8,23 +7,24 @@ import { ReactComponent as NxLogo } from '../../public/nx-logo-white.svg';
 
 import '../styles/global.scss';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to nextjs!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to nextjs!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </>
-  );
-}
+export const CustomApp: React.FunctionComponent<AppProps> = ({
+  Component,
+  pageProps,
+}) => (
+  <>
+    <Head>
+      <title>Welcome to nextjs!</title>
+    </Head>
+    <div className="app">
+      <header className="flex">
+        <NxLogo width="75" height="50" />
+        <h1>Welcome to nextjs!</h1>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  </>
+);
 
 export default CustomApp;
