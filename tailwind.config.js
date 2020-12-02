39a3205b20c874de162aa247001ff2adaf6cbc97
Erko Bridee <erko.bridee@gmail.com>
2020-12-02 16:42:02 +0100

    feat(tailwind): add the open sans font family to the root configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'open-sans': ['Open Sans'],
+      },
+    },
   },
   variants: {
     extend: {},
