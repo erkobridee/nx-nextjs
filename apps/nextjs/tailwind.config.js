f13c4c88c55bac11e5dff5efc46d3d50a24826fb
Erko Bridee <erko.bridee@gmail.com>
2020-12-04 13:59:32 +0100

    refactor(apps): code cleanup on the tailwind.config.js of the nextjs application

--- apps/nextjs/tailwind.config.js
+++ apps/nextjs/tailwind.config.js
@@ -1,10 +1,3 @@
-/*
-  the next nx plugin uses it
-  https://github.com/nrwl/nx/blob/98ed4b1dc7579680e9d4bf63274375d2d25216dc/packages/next/plugins/with-nx.ts#L2
-
-  source code
-  https://github.com/nrwl/nx/blob/98ed4b1dc7579680e9d4bf63274375d2d25216dc/packages/workspace/src/utils/app-root.ts
-*/
 const { appRootPath } = require('@nrwl/workspace/src/utils/app-root');
 
 const {
