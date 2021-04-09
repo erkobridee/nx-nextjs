5c81b4957dc4fbc4125e7a38f7e4d8d74081aee0
Erko Bridee <erko.bridee@gmail.com>
2021-04-09 14:43:50 +0200

    update(nx): 1st step of migration to v2

--- tools/config/webpack/nx-react.js
+++ tools/config/webpack/nx-react.js
@@ -1,3 +1,12 @@
+/*
+  the next nx plugin uses it
+  https://github.com/nrwl/nx/blob/98ed4b1dc7579680e9d4bf63274375d2d25216dc/packages/next/plugins/with-nx.ts#L2
+
+  source code
+  https://github.com/nrwl/nx/blob/98ed4b1dc7579680e9d4bf63274375d2d25216dc/packages/workspace/src/utils/app-root.ts
+*/
+const { appRootPath } = require('@nrwl/workspace/src/utils/app-root');
+
 const reactWebpackConfig = require('@nrwl/react/plugins/webpack');
 
 const { definitions, utils } = require('./helpers');
@@ -12,6 +21,11 @@ module.exports = (config) => {
     definitions.stylesWithPostcssRule,
     definitions.webfontsReactAppRule,
   ];
+
+  utils.addResolveAlias(config, {
+    // redefine the alias because the nextjs internally adds `./` to the CSS url() path
+    [`./@nx-nextjs/shared/assets/fonts`]: `${appRootPath}/libs/shared/assets/fonts`,
+  });
 
   return config;
 };
