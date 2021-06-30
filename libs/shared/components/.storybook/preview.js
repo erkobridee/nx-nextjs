3bf1cc0b17047646cb5e89a06edd1f39579dbde7
Erko Bridee <erko.bridee@gmail.com>
2021-06-30 11:03:58 +0200

    refactor(libs): cleanup storybook config of shared components lib

--- libs/shared/components/.storybook/preview.js
+++ libs/shared/components/.storybook/preview.js
@@ -1,10 +1,5 @@
-// import { addDecorator } from '@storybook/react';
-// import { withKnobs } from '@storybook/addon-knobs';
-
 import '@nx-nextjs/shared/assets/styles/fonts.scss';
 import '@nx-nextjs/shared/assets/styles/tailwind.scss';
-
-// addDecorator(withKnobs);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
