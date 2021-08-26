8ed6e00de86016c7fea098619ce2966ebc3fccb1
Erko Bridee <erko.bridee@gmail.com>
2021-08-26 12:49:19 +0200

    update(nx): 2nd step of migration to v12.8.0

--- libs/shared/components/.storybook/main.js
+++ libs/shared/components/.storybook/main.js
@@ -1,10 +1,81 @@
 const rootMain = require('../../../../.storybook/main');
+const {
+  addons: rootAddons,
+  stories: rootStories,
+  webpackFinal: rootWebpackFinal,
+} = rootMain;
 
-// Use the following syntax to add addons!
-//rootMain.addons.push('');
+module.exports = {
+  ...rootMain,
 
-rootMain.stories.push(
-  ...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']
-);
+  addons: [...rootAddons],
 
-module.exports = rootMain;
+  stories: [
+    ...rootStories,
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+
+  webpackFinal: async (config) => {
+    config = await rootWebpackFinal(config);
+
+    // Found this here: https://github.com/nrwl/nx/issues/2859
+    // And copied the part of the solution that made it work
+
+    const svgRuleIndex = config.module.rules.findIndex((rule) => {
+      const { test } = rule;
+
+      return test.toString().startsWith('/\\.(svg|ico');
+    });
+
+    config.module.rules[svgRuleIndex].test =
+      /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
+
+    config.module.rules.push(
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000, // 10kB
+          name: '[name].[hash:7].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        oneOf: [
+          // If coming from JS/TS file, then transform into React component using SVGR.
+          {
+            issuer: {
+              test: /\.[jt]sx?$/,
+            },
+            use: [
+              '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+              {
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000, // 10kB
+                  name: '[name].[hash:7].[ext]',
+                  esModule: false,
+                },
+              },
+            ],
+          },
+          // Fallback to plain URL loader.
+          {
+            use: [
+              {
+                loader: require.resolve('url-loader'),
+                options: {
+                  limit: 10000, // 10kB
+                  name: '[name].[hash:7].[ext]',
+                },
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    return config;
+  },
+};
