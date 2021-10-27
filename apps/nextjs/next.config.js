9ed1d140b4d49b89a674a52a68f656ddb84e805a
Erko Bridee <erko.bridee@gmail.com>
2021-10-27 17:43:54 +0200

    feat(nx): define ts-check target

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -2,6 +2,7 @@ const { appRootPath } = require('@nrwl/tao/src/utils/app-root');
 const withNx = require(`${appRootPath}/tools/config/nextjs`)(__dirname);
 
 module.exports = withNx({
+  swcMinify: true,
   images: {
     // workaround for nx v11 bug
     disableStaticImages: true,
